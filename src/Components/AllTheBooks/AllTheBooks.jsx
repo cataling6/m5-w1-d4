32040efd91f0e7077c1194ee180168a89eedff1e
Catalin Radoi <114146323+cataling6@users.noreply.github.com>
2024-02-12 21:08:19 +0100

    up

--- src/Components/AllTheBooks/AllTheBooks.jsx
+++ src/Components/AllTheBooks/AllTheBooks.jsx
@@ -1,63 +1,73 @@
 import { Card } from "react-bootstrap";
 import "./AllTheBooks.css"
 import { useState, useEffect } from "react";
-import "./fantasy.json"
+
+//import libri from "./fantasy.json"
 //https://epibooks.onrender.com/
 export const AllTheBooks = () => {
     const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState("");
+    const [searchValue, setSearchValue] = useState("");
+    console.log(searchValue);
 
-    // const getBooks = async () => {
-    //     try {
-    //         const result = await fetch("https://epibooks.onrender.com/history");
-    //         const data = await result.json();
+    const getBooks = async () => {
+        setLoading(true)
+        try {
+            const result = await fetch("https://epibooks.onrender.com/history");
+            const data = await result.json();
 
-    //         return setBooks(data);
+            setLoading(false)
+            setBooks(data);
 
-    //     } catch (e) {
-    //         console.log("erore", e);
-    //     }
-    // }
+        } catch (e) {
+            setError(e.message)
 
-    console.log(books);
+        }
+    }
+    //cercare il libro che contiene il valore di ricerca nel titolo, tutto in lowerCase
+    const filterBooks = (bookValue) => {
+        searchValue =
+            booksFiltered = books.filter((book) => book.title.toLowerCase().includes(bookValue.toLowerCase()))
+        setBooks(booksFiltered);
+    }
+
     useEffect(() => {
         getBooks();
     }, []);
 
-
-    const getBooks = () => {
-        try {
-            fetch('./scifi.json')
-                .then((resp) => resp.json())
-                .then((jsonData) => {
-                    setBooks(jsonData);
-                })
-                .catch((e) => {
-                    console.error("Errore", e);
-                })
-
-        } catch (e) {
-            console.log("erore", e);
-        }
-    }
-
-
     return (
 
         <div className="container ">
-            <div className="row d-flex gap-3">
-                {books.map(element => (
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={element.img} className="hover" />
-                        <Card.Body>
-                            <Card.Title className="text-truncate pointer" title={element.title}>{element.title}</Card.Title>
-                            <Card.Text className="d-flex justify-content-between">
-                                <span>{element.category}</span>
-                                <span>{element.price} €</span>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                ))}
-            </div>
-        </div>
+            {loading && error === "" && (
+                <>
+                    <div>Aspetta n'attimo...</div>
+                </>
+            )}
+            {error.length > 0 && (
+                <div>{error}</div>
+            )}
+            {!loading && error === "" && (
+                <> <div>nr totale libri: {books.length}</div>
+                    <input type="text" placeholder="Cerca il tuo libro" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+                    <button id="searchBtn" onClick={() => filterBooks(searchValue)}>Cerca</button>
+                    <div className="row d-flex gap-3">
+                        {books.map((element) => (
+                            <Card key={element.asin} style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={element.img} className="hover" />
+                                <Card.Body>
+                                    <Card.Title className="text-truncate pointer" title={element.title}>{element.title}</Card.Title>
+                                    <Card.Text className="d-flex justify-content-between">
+                                        <span>{element.category}</span>
+                                        <span>{element.price} €</span>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        ))}
+                    </div>
+                </>
+            )
+            }
+        </div >
     )
 }
