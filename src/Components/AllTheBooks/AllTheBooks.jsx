c4932061fb35ce120c60ae9b16b35dbcc00a5c5a
Catalin Radoi <radoi.catalin88@gmail.com>
2024-02-12 22:54:09 +0100

    up

--- src/Components/AllTheBooks/AllTheBooks.jsx
+++ src/Components/AllTheBooks/AllTheBooks.jsx
@@ -6,10 +6,12 @@ import { useState, useEffect } from "react";
 //https://epibooks.onrender.com/
 export const AllTheBooks = () => {
     const [books, setBooks] = useState([]);
+    const [booksTemp, setBooksTemp] = useState([]);
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState("");
     const [searchValue, setSearchValue] = useState("");
-    console.log(searchValue);
+    //const [controlSearch, setControlSearch] = useState(false);
+
 
     const getBooks = async () => {
         setLoading(true)
@@ -17,8 +19,9 @@ export const AllTheBooks = () => {
             const result = await fetch("https://epibooks.onrender.com/history");
             const data = await result.json();
 
-            setLoading(false)
+            setLoading(false);
             setBooks(data);
+            setBooksTemp(data);
 
         } catch (e) {
             setError(e.message)
@@ -27,9 +30,14 @@ export const AllTheBooks = () => {
     }
     //cercare il libro che contiene il valore di ricerca nel titolo, tutto in lowerCase
     const filterBooks = (bookValue) => {
-        searchValue =
-            booksFiltered = books.filter((book) => book.title.toLowerCase().includes(bookValue.toLowerCase()))
-        setBooks(booksFiltered);
+        setBooksTemp(books);
+        let booksFiltered = books.filter((book) => book.title.toLowerCase().includes(bookValue.toLowerCase()))
+        console.log(booksFiltered);
+        setBooksTemp(booksFiltered);
+
+    }
+    const verifySearch = (e) => {
+        setSearchValue(e.target.value)
     }
 
     useEffect(() => {
@@ -48,11 +56,11 @@ export const AllTheBooks = () => {
                 <div>{error}</div>
             )}
             {!loading && error === "" && (
-                <> <div>nr totale libri: {books.length}</div>
-                    <input type="text" placeholder="Cerca il tuo libro" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+                <> <div>nr totale libri: {booksTemp.length}</div>
+                    <input type="text" placeholder="Cerca il tuo libro" value={searchValue} onChange={(e) => verifySearch(e)} />
                     <button id="searchBtn" onClick={() => filterBooks(searchValue)}>Cerca</button>
                     <div className="row d-flex gap-3">
-                        {books.map((element) => (
+                        {booksTemp.map((element) => (
                             <Card key={element.asin} style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src={element.img} className="hover" />
                                 <Card.Body>
